f26438974006627cd0173083759828b720dcac42
sqljared <sqljared@outlook.com>
2025-03-29 14:24:21 -0400

    OneBite: Updates to examples and slides

--- 00_WWI_Restore.sql
+++ 00_WWI_Restore.sql
@@ -1,5 +1,11 @@
 USE [master]
 RESTORE DATABASE [WideWorldImporters] 
-	FROM  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQL2019\MSSQL\Backup\WWI-Larger.bak' 
-	WITH  FILE = 1,  NOUNLOAD,  STATS = 5;
+	FROM  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\WideWorldImporters-Full.bak' WITH  FILE = 1,  
+	MOVE N'WWI_Primary' TO N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\WideWorldImporters.mdf',  
+	MOVE N'WWI_UserData' TO N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\WideWorldImporters_UserData.ndf',  
+	MOVE N'WWI_Log' TO N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\WideWorldImporters.ldf',  
+	MOVE N'WWI_InMemory_Data_1' TO N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\WideWorldImporters_InMemory_Data_1',  
+	NOUNLOAD,  STATS = 5
+GO
+ALTER DATABASE [WideWorldImporters] SET COMPATIBILITY_LEVEL = 150
 GO
--- 12-TopWrinkle.sql
+++ 12-TopWrinkle.sql
@@ -4,7 +4,7 @@ SELECT TOP 100
 	sol.OrderID, 
 	sol.UnitPrice,
 	sol.Description
-FROM Sales.OrderLines sol 
+FROM Sales.OrderLines sol WITH(INDEX(FK_Sales_OrderLines_OrderID))
 WHERE 
 	sol.OrderID < 1000 
 	AND sol.Quantity > 5
